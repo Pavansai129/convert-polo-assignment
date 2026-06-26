451593a90853f56e49fa8b0d022ab6f7825ef095
Pavan Sai <pavansaivvit@gmail.com>
2026-06-26 13:36:12 +0530

    Update the faq item icons to the corresponding category and fix the support widget screen transition

--- assets/support-widget.js
+++ assets/support-widget.js
@@ -405,12 +405,7 @@ class SupportWidget extends HTMLElement {
         faqButton.dataset.categoryId    = faq.categoryId;
         faqButton.setAttribute('role', 'listitem');
         faqButton.innerHTML = `
-          <span class="sw-faq-item__icon" aria-hidden="true">
-            <svg viewBox="0 0 24 24" fill="none" stroke="currentColor"
-                  stroke-width="1.8" stroke-linecap="round" stroke-linejoin="round">
-              <path d="M5 8h14M5 8a2 2 0 1 0 0-4h14a2 2 0 1 0 0 4M5 8v10a2 2 0 0 0 2 2h10a2 2 0 0 0 2-2V8"/>
-            </svg>
-          </span>
+          <span class="sw-faq-item__icon">${categoryData.icon}</span>
           <span>
             <p class="sw-faq-item__question">${this._escapeHtml(faq.question)}</p>
             <p class="sw-faq-item__category">${this._escapeHtml(faq.categoryLabel)}</p>
